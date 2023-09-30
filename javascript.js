a8f98ab5a01cf0a6a1e433f54bed5f69c047698e
shannoj <69207315+shannoj@users.noreply.github.com>
2023-09-30 10:26:22 +0200

    updated the checkWinner function and added players to the game

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 
 // player.js
-export const Player = (name, symbol) => {
+const Player = (name, symbol) => {
     return {
         name,
         symbol,
@@ -8,36 +8,50 @@ export const Player = (name, symbol) => {
 };
 
 // game.js
-export const Game = () => {
+const Game = (player1, player2) => {
     const board = ["", "", "", "", "", "", "", "", ""];
-    let currentPlayer = null;
+    let currentPlayer = player1;
     let winner = null;
 
     const checkWinner = () => {
         // Implement the logic to check for a winner
-        if ((board[0] && board[1] && board[2] === currentPlayer.symbol) || (board[3] && board[4] && board[5] === currentPlayer.symbol) || (board[6] && board[7] && board[8] === currentPlayer.symbol)){
-            winner == currentPlayer;
+        const winConditions = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+            [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+            [0, 4, 8], [2, 4, 6],            // Diagonals
+        ];
+    
+        for (const condition of winConditions) {
+            const [a, b, c] = condition;
+            if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+                // We have a winner
+                winner = currentPlayer;
+                return;
+            }
         }
-
-        if ((board[0] && board[3] && board[6] === currentPlayer.symbol) || (board[1] && board[4] && board[7] === currentPlayer.symbol) || (board[2] && board[5] && board[8] === currentPlayer.symbol)){
-            winner == currentPlayer;
-        }
-
-        if ((board[0] && board[4] && board[8] === currentPlayer.symbol) || (board[2] && board[4] && board[6] === currentPlayer.symbol)){
-            winner == currentPlayer;
+    
+        // Check for a draw
+        if (!board.includes("")) {
+            // No winner, the game is a draw
+            winner = null;
         }
     };
 
     const makeMove = (index) => {
         if (!board[index] && !winner) {
             board[index] = currentPlayer.symbol;
-            currentPlayer = currentPlayer === player1 ? player2 : player1;
             checkWinner();
+            currentPlayer = currentPlayer === player1 ? player2 : player1;
         }
     };
 
     const getCurrentPlayer = () => {
         // Implement the logic to get the current player
+        return currentPlayer;
+    };
+
+    const getWinner = () => {
+        return winner;
     };
 
     return {
@@ -48,13 +62,18 @@ export const Game = () => {
 };
 
 const squares = document.querySelectorAll(".square");
-let currentPlayer = 'X';
 
-squares.forEach(square => {
+const player1 = Player('James', 'X');
+
+const player2 = Player('Devon', 'O');
+
+const game = Game(player1, player2);
+
+squares.forEach((square, index) => {
     square.addEventListener('click', () => {
-        if (square.textContent === '') {
-            square.textContent = currentPlayer;
-            currentPlayer = currentPlayer === "X" ? "O" : "X";
+        if (!square.textContent &&  !game.getWinner()) {
+            square.textContent = game.getCurrentPlayer().symbol;
+            game.makeMove(index);
         }
     })
 })

391053e3aa6bfd9a74390105bc39d5e14a829314
shannoj <69207315+shannoj@users.noreply.github.com>
2023-09-30 09:52:41 +0200

    made the game and player functions and wrote the check winner function and make move function

--- javascript.js
+++ javascript.js
@@ -1,3 +1,52 @@
+
+// player.js
+export const Player = (name, symbol) => {
+    return {
+        name,
+        symbol,
+    };
+};
+
+// game.js
+export const Game = () => {
+    const board = ["", "", "", "", "", "", "", "", ""];
+    let currentPlayer = null;
+    let winner = null;
+
+    const checkWinner = () => {
+        // Implement the logic to check for a winner
+        if ((board[0] && board[1] && board[2] === currentPlayer.symbol) || (board[3] && board[4] && board[5] === currentPlayer.symbol) || (board[6] && board[7] && board[8] === currentPlayer.symbol)){
+            winner == currentPlayer;
+        }
+
+        if ((board[0] && board[3] && board[6] === currentPlayer.symbol) || (board[1] && board[4] && board[7] === currentPlayer.symbol) || (board[2] && board[5] && board[8] === currentPlayer.symbol)){
+            winner == currentPlayer;
+        }
+
+        if ((board[0] && board[4] && board[8] === currentPlayer.symbol) || (board[2] && board[4] && board[6] === currentPlayer.symbol)){
+            winner == currentPlayer;
+        }
+    };
+
+    const makeMove = (index) => {
+        if (!board[index] && !winner) {
+            board[index] = currentPlayer.symbol;
+            currentPlayer = currentPlayer === player1 ? player2 : player1;
+            checkWinner();
+        }
+    };
+
+    const getCurrentPlayer = () => {
+        // Implement the logic to get the current player
+    };
+
+    return {
+        makeMove,
+        getCurrentPlayer,
+        getWinner,
+    };
+};
+
 const squares = document.querySelectorAll(".square");
 let currentPlayer = 'X';
 
@@ -9,3 +58,5 @@ squares.forEach(square => {
         }
     })
 })
+
+
